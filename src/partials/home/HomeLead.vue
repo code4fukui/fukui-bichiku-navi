20c364d4b5b0a9dbcb57a78538909768c61f5943
Taisuke Fukuno <fukuno@jig.jp>
2022-03-11 08:02:41 +0900

    備蓄カードへのリンク追加

--- src/partials/home/HomeLead.vue
+++ src/partials/home/HomeLead.vue
@@ -5,14 +5,16 @@
     "text01": "「災害に備えた備蓄」と聞いてもピンと来ない方や興味はあるけど何をどのくらい備蓄すれば良いか分からない方向けに、備蓄のイロハや備えておくと良い品目などをご紹介するサイトです。",
     "text02": "今すぐ備蓄を<br />始める！",
     "text03": "災害対策ゲーム<br />「サイガイクン」で学ぶ！",
-    "text04": "越前市避難所マップを<br>見る！"
+    "text04": "備蓄品カードを<br>印刷して遊ぶ！",
+    "text05": "越前市避難所マップを<br>見る！"
   },
   "en": {
     "title": "Prepare for the future disaster",
     "text01": "For some people, the words “stockpiling for possible disasters” do not ring a bell and, even those who are interested in stockpiling might find it difficult to figure out what and how much they should stockpile. Tokyo Stockpile Navi website provides an introductory explanation of stockpiling including items recommended for stockpiles.",
     "text02": "Start &quot;stockpiling&quot; NOW!",
     "text03": "Learn with \"Saigaikun\"<br />survival game!",
-    "text04": "Open Hazard Map<br>in Echizen city!"
+    "text04": "Play with Stockpile<br />card game!",
+    "text05": "Open Hazard Map<br>in Echizen city!"
   }
 }
 </i18n>
@@ -65,8 +67,18 @@
         <div class="Button -yellow -l">
           <a
             class="Button__link"
-            href="https://code4fukui.github.io/echizencity-hazardmap/"
+            href="opendata/card.html"
             v-html="$t('text04')"
+          />
+        </div>
+      </div>
+
+      <div class="HomeLead__button">
+        <div class="Button -yellow -l">
+          <a
+            class="Button__link"
+            href="https://code4fukui.github.io/echizencity-hazardmap/"
+            v-html="$t('text05')"
           />
         </div>
       </div>
