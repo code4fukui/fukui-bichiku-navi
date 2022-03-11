fe6aed915586051c1803b312016ef42ceb473754
Taisuke Fukuno <fukuno@jig.jp>
2022-03-11 15:58:03 +0900

    add ogp and link

--- src/partials/GlobalFooter.vue
+++ src/partials/GlobalFooter.vue
@@ -2,6 +2,7 @@
   <footer class="GlobalFooter">
     <div class="GlobalFooter__admin">
       <div>{{ $developper[$i18n.locale] }}</div>
+      <div><a href="https://github.com/code4fukui/fukui-bichiku-navi">オープンソース (src on GitHub)</a></div>
     </div>
   </footer>
 </template>
